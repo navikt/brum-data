9c87e218e42c84a43dda7f184417c26fe768cd8f
Aksel Lessum <aksel.henrik.lessum@nav.no>
2025-06-26 14:33:29 +0200

    First attempt at DAG setup for data collection

---
+++ dags/daily_raw_data_migrate.py
@@ -0,0 +1,33 @@
+"""DAG to automate the collection of the raw data from other BQ projects to Brum"""
+from datetime import datetime
+from airflow import DAG
+from dataverk_airflow import python_operator
+
+allowlist = [
+    "secretmanager.googleapis.com",
+    "bigquery.googleapis.com",
+    
+]
+
+with DAG(
+    dag_id="daily_raw_data_migrate",
+    description="A DAG that migrates data from different BQ projects to the brum project",
+    owner="brum-data",
+    schedule_interval="0 6 * * *",
+    start_date=datetime(2025, 6, 27, tzinfo=("Europe/Oslo")),
+    catchup=False
+) as dag:
+    migrate_bigquery = python_operator(
+        dag=dag,
+        name="migrate_bigquery",
+        repo="navikt/brum-data",
+        script_path="src/data_collection/transfer_bq_datasets.py",
+        requirements_path="requirements_bq.txt",
+        use_uv_pip_install=True,
+        slack_channel="brum-intern",
+        allowlist=allowlist
+    )
+
+    migrate_bigquery
+
+
