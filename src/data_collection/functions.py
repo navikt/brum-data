c7a29ae1d6f789ec7b487ca4be749a306c5cc70f
Aksel Lessum <aksel.henrik.lessum@nav.no>
2025-07-04 14:51:20 +0200

    Added type hinting to functions

--- src/data_collection/functions.py
+++ src/data_collection/functions.py
@@ -11,20 +11,20 @@ from google.oauth2 import service_account
 from google.cloud import bigquery
 from google.cloud import secretmanager
 
-def _get_secret(project_id, sa_key_name='service-account-key'):
+def _get_secret(project_id: str, sa_key_name='service-account-key'):
     client = secretmanager.SecretManagerServiceClient()
     name = f"projects/{project_id}/secrets/{sa_key_name}/versions/latest"
     response = client.access_secret_version(request={"name": name})
     secret = json.loads(response.payload.data.decode('UTF-8'))
     return secret
 
-def create_client(project_id, sa_key_name):
+def create_client(project_id: str, sa_key_name: str):
     key = _get_secret(project_id, sa_key_name)
     creds = service_account.Credentials.from_service_account_info(key)
     client = bigquery.Client(credentials=creds, project=creds.project_id)
     return client
 
-def write_to_BQ(client, table_name, dframe, dataset='Kaggle_test_data', disp = "WRITE_APPEND", schema_name='schema.json'):
+def write_to_BQ(client: bigquery.Client, table_name: str, dframe: pd.DataFrame, dataset='Kaggle_test_data', disp = "WRITE_APPEND", schema_name='schema.json'):
     with open(schema_name, 'rb') as f:
         schema = json.load(f)
 
@@ -39,7 +39,7 @@ def write_to_BQ(client, table_name, dframe, dataset='Kaggle_test_data', disp = "
         dframe, table_id, job_config=job_config
     )
 
-def get_data_from_BQ(bq_client, TARGET_PROJECT_ID, DATASET, source_table):
+def get_data_from_BQ(bq_client: bigquery.Client, TARGET_PROJECT_ID: str, DATASET: str, source_table: str):
     # Read data from BigQuery
     sql_src_qry = f"SELECT * FROM `{TARGET_PROJECT_ID}.{DATASET}.{source_table}`"
 
@@ -48,7 +48,7 @@ def get_data_from_BQ(bq_client, TARGET_PROJECT_ID, DATASET, source_table):
 
     return df
 
-def update_raw_data(client, query):
+def update_raw_data(client: bigquery.Client, query: str):
     logging.info(f"Attempting to get raw data from BQ")
     try:
         client.query(query)
